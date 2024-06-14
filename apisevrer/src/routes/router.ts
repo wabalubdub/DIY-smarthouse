76e8cbe3d4863e975466d14bfcc470e15e260c4e
wabalubdub <noamboam2@gmail.com>
2024-06-14 20:22:28 +0300

    restart the project

--- apisevrer/src/routes/router.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import configRouter from "./config";
-
-const router = express.Router();
-
-// Mount routes
-router.use("/config", configRouter);
-
-export default router;
